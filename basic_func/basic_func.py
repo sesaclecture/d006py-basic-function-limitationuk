a88811d3e97b0d43904c7af9edb84b33da71a818
jeonguk <wjddnr1358@naver.com>
2025-09-02 11:34:16 +0900

    과제 제출합니다

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,27 @@
+#사칙연산
 def add(a, b):
-    pass
-
-
+    return a + b
 def sub(a, b):
-    pass
-
-
-def mul(a, b):
-    pass
-
-
-def div(a, b):
-    pass
-
-
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+    
+#지수연산
 def power(base, pow):
-    pass
-
-
-def square(base):
-    pass
-
+    return base**pow
+def square (base):
+    return power(base, 2)
+def cube (base):
+    return power(base, 3)
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    인사 = ""
+    if (나이>=40):
+        인사 ="안녕하십니까"
+    elif (나이>=20):
+        인사 ="안녕하신가"
+    else:
+        인사 = "안녕"
+    return 인사 + f" {이름}!"    
